97ede3172bbb70a07995881d2cce854e45b9499e
Casper Guo <casperg@umich.edu>
2024-01-22 11:19:26 -0500

    Undo open ETL log in notebook

--- trigger.bat
+++ trigger.bat
@@ -1,5 +1,4 @@
 wsl.exe chmod +x /mnt/d/Projects/F1-Visualization/data-refresh.sh
 wsl.exe chmod +x /mnt/d/Projects/F1-Visualization/auto-push.exp
 wsl.exe /mnt/d/Projects/F1-Visualization/data-refresh.sh
-start Notepad "ETL.log"
 timeout 60
