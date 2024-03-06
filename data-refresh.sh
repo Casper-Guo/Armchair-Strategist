108d6b5259188cb3500df2c838655c3b13a2e71b
Casper Guo <casperg@umich.edu>
2024-03-06 11:55:13 -0500

    Use stash instead of creating a save commit when checking out main
    Ignore non-zero return code for git commit when there's no diff on main

--- data-refresh.sh
+++ data-refresh.sh
@@ -17,15 +17,20 @@ PASSWORD=$(cat .password)
 
 if [ $BRANCH != "main" ]
 then
-    git add .
-    git commit -m "Save progress before checkout"
+    git stash -u
     git checkout main
 fi
 
 python3 src/preprocess.py
 python3 src/readme_update.py >/dev/null
 git add .
-git commit -m "Automatic data refresh"
+git commit -m "Automatic data refresh" || true # ignore non-zero exit status when there's no diff on main 
 ./auto-push.exp "$PASSWORD" -d
-git checkout "$BRANCH"
+
+if [ $BRANCH != "main" ]
+then
+    git checkout "$BRANCH"
+    git stash pop || true # ignore non-zero exit status when there's no diff on branch
+fi
+
 echo "Success"
