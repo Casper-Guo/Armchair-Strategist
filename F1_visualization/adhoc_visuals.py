9ada5baa14c52bf423e3794f5700e3d6c72e5b1a
Casper Guo <casperg@umich.edu>
2024-07-04 16:02:37 -0400

    Standardize directory names

---
+++ F1_visualization/adhoc_visuals.py
@@ -0,0 +1,32 @@
+"""Visualization playground."""
+
+import warnings
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+mpl.use("Agg")
+# uncomment this
+# import f1_visualization.visualization as viz
+
+
+def main():
+    """Set up visualizations."""
+    # plotting setup
+    sns.set_theme(rc={"figure.dpi": 300, "savefig.dpi": 300})
+    plt.style.use("dark_background")
+
+    # Suppress pandas SettingWithCopy warning
+    pd.options.mode.chained_assignment = None
+
+    # Suppress Seaborn false positive warnings
+    # TODO: This is dangerous
+    warnings.filterwarnings("ignore")
+
+    # Visualizations below
+
+
+if __name__ == "__main__":
+    main()
