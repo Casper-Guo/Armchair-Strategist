b6a9e03a1a812e5c60e14d308768ba5b9fc36371
Casper Guo <casperg@umich.edu>
2025-02-09 01:09:20 -0500

    Create graph legends tab

--- f1_visualization/plotly_dash/layout.py
+++ f1_visualization/plotly_dash/layout.py
@@ -298,6 +298,87 @@ compound_plot_tab = dbc.Tab(
     label="Compound Performance Plot",
 )
 
+compound_color_scheme_card = dbc.Card(
+    [
+        dbc.CardHeader(html.B("Compounds Color Scheme"), style={"textAlign": "center"}),
+        dbc.ListGroup(
+            [
+                dbc.ListGroupItem(
+                    html.B("SOFT"),
+                    color="#da291c",
+                    style={"textAlign": "center"},
+                ),
+                dbc.ListGroupItem(
+                    html.B("MEDIUM"),
+                    color="#ffd12e",
+                    style={"textAlign": "center"},
+                ),
+                dbc.ListGroupItem(
+                    html.B("HARD"),
+                    color="#f0f0ec",
+                    style={"textAlign": "center"},
+                ),
+                dbc.ListGroupItem(
+                    html.B("INTERMEDIATE"),
+                    color="#43b02a",
+                    style={"textAlign": "center"},
+                ),
+                dbc.ListGroupItem(
+                    html.B("WET"),
+                    color="#0067ad",
+                    style={"textAlign": "center"},
+                ),
+                dbc.ListGroupItem(
+                    html.B("UNKNOWN"),
+                    color="#00ffff",
+                    style={"textAlign": "center"},
+                ),
+            ],
+        ),
+    ],
+)
+
+fresh_used_scheme_card = dbc.Card(
+    [
+        dbc.Accordion(
+            [
+                dbc.AccordionItem(
+                    dbc.Progress(
+                        value=100,
+                        color="warning",
+                        style={"height": "40px"},
+                    ),
+                    title="Fresh Tyre Bar",
+                ),
+                dbc.AccordionItem(
+                    html.P("⚫", style={"textAlign": "center"}),
+                    title="Fresh Tyre Marker",
+                ),
+                dbc.AccordionItem(
+                    dbc.Progress(
+                        value=100,
+                        color="warning",
+                        striped=True,
+                        style={"height": "40px"},
+                    ),
+                    title="Used Tyre Bar (Striped)",
+                ),
+                dbc.AccordionItem(
+                    html.P(html.B("X"), style={"textAlign": "center"}),
+                    title="Used Tyre Marker",
+                ),
+            ],
+            start_collapsed=True,
+            always_open=True,
+        ),
+    ]
+)
+
+legends_tab = dbc.Tab(
+    [compound_color_scheme_card, html.Br(), fresh_used_scheme_card],
+    label="Graph Legends",
+)
+
 external_links = dbc.Alert(
     [
         "Made by ",
@@ -347,7 +428,14 @@ app_layout = dbc.Container(
         add_gap_row,
         html.Br(),
         dbc.Tabs(
-            [strategy_tab, scatterplot_tab, lineplot_tab, distplot_tab, compound_plot_tab]
+            [
+                strategy_tab,
+                scatterplot_tab,
+                lineplot_tab,
+                distplot_tab,
+                compound_plot_tab,
+                legends_tab,
+            ]
         ),
         html.Br(),
         dbc.Row(external_links),
