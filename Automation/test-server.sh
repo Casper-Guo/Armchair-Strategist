e61f82e09f4b29319a0cde83720cd78e36f278f2
Casper Guo <casperg@umich.edu>
2025-02-17 12:02:48 -0500

    Server health check script attaches Dash log

--- Automation/test-server.sh
+++ Automation/test-server.sh
@@ -4,5 +4,5 @@ if ! lsof -i :8000
 then
     UTC=$(date)
     bash ~/Armchair-Strategist/Automation/start-server.sh
-    aws sns publish --topic-arn arn:aws:sns:us-east-2:637423600104:Armchair-Strategist --message "WARNING: The server had been stopped in the last 30 minutes" --subject "Server Health Warning - $UTC"
+    aws sns publish --topic-arn arn:aws:sns:us-east-2:637423600104:Armchair-Strategist --message file://~/Armchair-Strategist/Automation/dash.log --subject "Server Health Warning - $UTC"
 fi
